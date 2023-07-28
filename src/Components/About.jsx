4d7442bae62a81fddbd3cc83f599de8677ee9c4e
Yuvraj Singh <ys2002github@gmail.com>
2023-07-28 16:53:55 +0000

    Added my description and other related information

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,17 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "AI/ML Developer | Intern Wictronix | Published Researcher (IEEE Bangalore) | MLSA IIT Mandi Frost Hackathon Winner | Award-Winner Blogger ";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Machine learning",
+  "Deep learning",
+  "Computer vision",
+  "Natural language processing",
 ];
 
 /**
@@ -45,7 +43,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation.";
 
 const About = () => {
   return (
